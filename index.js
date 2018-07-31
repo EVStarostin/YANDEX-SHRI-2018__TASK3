98d3dde1868fcce0728e8b2946e098a2fc5fafb6
Starostin Evgenii <estarostin@bellintegrator.com>
2018-07-31 10:28:17 +0300

    -

--- index.js
+++ index.js
@@ -72,8 +72,8 @@ function makeSchedule(inputData) {
       rates = inputData.rates,
       maxPower = inputData.maxPower;
 
-  var day = [7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
-      night = [21, 22, 23, 0, 1, 2, 3, 4, 5, 6];
+  // var day = [7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
+  //     night = [21, 22, 23, 0, 1, 2, 3, 4, 5, 6];
 
   devices = devices.sort( (a, b) => b.power - a.power );
   // console.log(devices.forEach( (el) => console.log(el.name) ));
