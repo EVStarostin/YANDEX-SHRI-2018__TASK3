daf4395cfaf486f211a0a19718c1cf0482146061
Starostin Evgenii <estarostin@bellintegrator.com>
2018-08-01 17:46:20 +0300

    -

--- test.js
+++ test.js
@@ -6,8 +6,6 @@ var assert = require('chai').assert
 describe('consumedEnergy', function () {
   describe('value', function () {
     it('should be is less than or equal to 38.939', function () {
-      // assert.equal([1,2,3].indexOf(4), -1);
-      // assert.equal(JSON.parse(makeSchedule(inputData)).consumedEnergy.value, 38.939);
       assert.isAtMost(JSON.parse(makeSchedule(inputData)).consumedEnergy.value, 38.939)
     })
   })
